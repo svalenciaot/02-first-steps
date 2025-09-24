378d4a31d614f159973a1dc37a63bfa0760a2f3f
Santiago Valencia <svalencia@econtainers.co>
2025-09-24 18:29:25 -0500

    09-24 styles

--- src/MyAwesomeApp.tsx
+++ src/MyAwesomeApp.tsx
@@ -1,8 +1,31 @@
-export function Nombre(){
+import type { CSSProperties } from "react";
+
+const firstName = 'Santiago';
+const lastName = 'Valencia';
+const favoriteGames = ['Halo', 'Mario Kart', 'AOE'];
+const isActive = false;
+
+const address = {
+    zipCode: 123,
+    country: 'Canadá',
+}
+
+const myStyles: CSSProperties = {
+    backgroundColor: isActive ? 'grey' : 'red',
+    borderRadius: 10,
+    padding: 10
+}
+
+export function Nombre() {
     return (
         <>
-            <h1>Santiago</h1>
-            <h3>Valencia</h3>
+            <h1>{firstName}</h1>
+            <h3>{lastName}</h3>
+            <h3>{favoriteGames.join(', ')}</h3>
+            <h1>{isActive ? 'Activo' : 'No Activo'}</h1>
+            <p style={myStyles}>
+                {JSON.stringify(address)}
+            </p>
         </>
     )
 }
